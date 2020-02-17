43724360cd58bc70a1ba25a38bbed272bb0d37f0
AnatoliyShumov <shumov@radioday.kpi.ua>
2020-02-18 00:41:20 +0200

    Updates

---
+++ precache-manifest.8154603d113cb9c4bf4292bfa25dde4b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177ce0631d22ec535ed8298e53c5b439",
+    "url": "/join-to-it/build/index.html"
+  },
+  {
+    "revision": "abd88f3117d40afaf6b2",
+    "url": "/join-to-it/build/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "04954c0ab6bdd501773f",
+    "url": "/join-to-it/build/static/js/2.8f3e07de.chunk.js"
+  },
+  {
+    "revision": "ac5d9a2ccd3ba848587d2a460762f2f0",
+    "url": "/join-to-it/build/static/js/2.8f3e07de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd88f3117d40afaf6b2",
+    "url": "/join-to-it/build/static/js/main.8fa078ed.chunk.js"
+  },
+  {
+    "revision": "8d8ac2ab21248cd55481",
+    "url": "/join-to-it/build/static/js/runtime-main.f3d3d78c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/join-to-it/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2395eadc34cc7965d2c412152fa06a9b",
+    "url": "/join-to-it/build/static/media/moon.2395eadc.svg"
+  },
+  {
+    "revision": "03b952a4c1374d5b105b411e693bba1d",
+    "url": "/join-to-it/build/static/media/sun.03b952a4.svg"
+  }
+]);
