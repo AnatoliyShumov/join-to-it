86fff3e01e4dd06327402be27352ecb0f39b671d
AnatoliyShumov <shumov@radioday.kpi.ua>
2020-02-18 00:45:50 +0200

    Updates

---
+++ precache-manifest.7f97d2b9c3fdb7f7ae49bc9e8fb38d43.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4427dc4e6cae24aa31c8682f6351db",
+    "url": "./https://anatoliyshumov.github.io/join-to-it/index.html"
+  },
+  {
+    "revision": "b7498d803196aee968cf",
+    "url": "./https://anatoliyshumov.github.io/join-to-it/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "288a6251b70b114bea6c",
+    "url": "./https://anatoliyshumov.github.io/join-to-it/static/js/2.9c604838.chunk.js"
+  },
+  {
+    "revision": "ac5d9a2ccd3ba848587d2a460762f2f0",
+    "url": "./https://anatoliyshumov.github.io/join-to-it/static/js/2.9c604838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7498d803196aee968cf",
+    "url": "./https://anatoliyshumov.github.io/join-to-it/static/js/main.45a1c285.chunk.js"
+  },
+  {
+    "revision": "8b33157d6b8462fddf41",
+    "url": "./https://anatoliyshumov.github.io/join-to-it/static/js/runtime-main.46b57d9d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./https://anatoliyshumov.github.io/join-to-it/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2395eadc34cc7965d2c412152fa06a9b",
+    "url": "./https://anatoliyshumov.github.io/join-to-it/static/media/moon.2395eadc.svg"
+  },
+  {
+    "revision": "03b952a4c1374d5b105b411e693bba1d",
+    "url": "./https://anatoliyshumov.github.io/join-to-it/static/media/sun.03b952a4.svg"
+  }
+]);
